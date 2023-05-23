b1bdd70fd5952f6279328c7a2ead5b77b2858619
8ctopus <hello@octopuslabs.io>
2023-05-23 11:55:11 +0400

    Improve tests

--- tests/QuotedPrintableTest.php
+++ tests/QuotedPrintableTest.php
@@ -100,6 +100,39 @@ final class QuotedPrintableTest extends TestCase
                 J'interdis aux marchands de vanter trop leurs marchandises. Car ils se fo=20
                  vite p=C3=A9dagogues et t'enseignent comme but ce qui n'est par essence qu=
                 TEXT,
+            ], [
+                'text' => <<<TEXT
+                Quoted-printable encoding=0AAny 8-bit byte value may be encoded with 3 char=
+                acters: an =3D followed by two hexadecimal digits (0=E2=80=939 or A=
+                =E2=80=93F) representing the byte's numeric value. For example, an ASCII fo=
+                rm feed character (decimal value 12) can be represented by =3D0C, and an AS=
+                CII equal sign (decimal value 61) must be represented by =3D3D. All charact=
+                ers except printable ASCII characters or end of line characters (but also =
+                =3D) must be encoded in this fashion.=0A=0AAll printable ASCII characters (=
+                decimal values between 33 and 126) may be represented by themselves, except=
+                 =3D (decimal 61, hexadecimal 3D, therefore =3D3D).=0A=0AASCII tab and spac=
+                e characters, decimal values 9 and 32, may be represented by themselves, ex=
+                cept if these characters would appear at the end of the encoded line. In th=
+                at case, they would need to be escaped as =3D09 (tab) or =3D20 (space), or =
+                be followed by a =3D (soft line break) as the last character of the encoded=
+                 line. This last solution is valid because it prevents the tab or space fro=
+                m being the last character of the encoded line.=0A=0AIf the data being enco=
+                ded contains meaningful line breaks, they must be encoded as an ASCII CR LF=
+                 sequence, not as their original byte values, neither directly nor via =3D =
+                signs. Conversely, if byte values 13 and 10 have meanings other than end of=
+                 line (in media types,[2] for example), then they must be encoded as =3D0D =
+                and =3D0A respectively.=0A=0ALines of Quoted-Printable encoded data must no=
+                t be longer than 76 characters. To satisfy this requirement without alterin=
+                g the encoded text, soft line breaks may be added as desired. A soft line b=
+                reak consists of an =3D at the end of an encoded line, and does not appear =
+                as a line break in the decoded text. These soft line breaks also allow enco=
+                ding text without line breaks (or containing very long lines) for an enviro=
+                nment where line size is limited, such as the 1000 characters per line limi=
+                t of some SMTP software, as allowed by RFC 2821.=0A=0AA slightly modified v=
+                ersion of Quoted-Printable is used in message headers; see MIME#Encoded-Wor=
+                d.
+
+                TEXT,
             ]
         ];
     }
